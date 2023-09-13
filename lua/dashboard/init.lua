1adb53a0ccf8bf63d2831970f9e3ff77d579d008
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-12 23:45:50 -0700

    Get some lines writing, make exiting easy

--- lua/dashboard/init.lua
+++ lua/dashboard/init.lua
@@ -1,7 +1,24 @@
 local M = {}
 
 M.instance = function()
-    print('HERE WE ARE')
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    print('MODE', vim.api.nvim_get_mode().mode)
+    print('MODIFIED', vim.bo.modified)
+    print('BUF_NUMBER', bufnr)
+
+    local fill = {}
+    table.insert(fill, 'test-1')
+    table.insert(fill, 'test-2')
+    table.insert(fill, 'test-3')
+
+    print('LINES', vim.api.nvim_buf_line_count(bufnr))
+    print('FILL_SIZE', #fill)
+
+    vim.api.nvim_buf_set_lines(bufnr, 0, 0, false, fill)
+
+    vim.bo[bufnr].modifiable = false
+    vim.bo[bufnr].modified = false
 end
 
 M.setup = function(opts)

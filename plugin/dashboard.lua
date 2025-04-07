74cad8c427df66825ee9fb10be37424743c3d8bf
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-04-07 16:49:57 -0700

    chore: remove default clear argument

--- plugin/dashboard.lua
+++ plugin/dashboard.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd('UIEnter', {
-    group = vim.api.nvim_create_augroup('Dashboard', { clear = true }),
+    group = vim.api.nvim_create_augroup('Dashboard', {}),
     callback = function()
         if vim.fn.argc() == 0 then
             require('dashboard').instance()
